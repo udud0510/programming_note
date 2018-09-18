469e70d59cdfda0ad0e1f9d0b8c02ef499e52333
BuildTools <unconfigured@null.spigotmc.org>
2018-09-18 20:23:53 +0900

    [프로그래머스 level 1] 문자열 다루기 기본 : 동작완료

--- sujin/programmers/level1/Solution.java
+++ sujin/programmers/level1/Solution.java
@@ -1,8 +1,27 @@
 package level1;
 
 public class Solution {
-    public boolean solution(String s) {
-        boolean answer = true;
+    public static boolean solution(String s) {
+        boolean answer;
+
+        if(s.length()==4 || s.length()==6 ){
+            try {
+                Integer.parseInt(s);
+                answer=true;
+            } catch (NumberFormatException e){
+                answer=false;
+            }
+        }
+        else{
+            answer = false;
+        }
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution("55"));
+
     }
 }
