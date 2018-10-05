66d5cce6945837b0431c82b8f7f42696fbc6d52e
AsCE-hyunseung <leehs1222@gmail.com>
2018-10-05 17:56:37 +0900

    [정렬] H - index

---
+++ programmers/정렬/H - index/HIndexTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HIndexTest {
+    HIndex h= new HIndex();
+    int []r={3,0,6,1,5};
+    int []r1={2,6,5,4,2,0,4};
+
+    @Test
+    public void solution() {
+
+        assertEquals(3,h.solution(r));
+        assertEquals(4,h.solution(r1));
+    }
+}
