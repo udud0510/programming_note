c18edb97829c579b9862acb361db215c192b59c1
jongholee <jhleed3@gmail.com>
2018-10-06 08:44:55 +0900

    programmers : hash : 완주하지 못한 선수 : Map, Set API 테스트

--- jongholee/programmers/hash/java/src/완주하지_못한_선수/Solution.java
+++ jongholee/programmers/hash/java/src/완주하지_못한_선수/Solution.java
@@ -2,13 +2,16 @@ package 완주하지_못한_선수;
 
 public class Solution {
 
-    public String solution(String[] p, String[] c) {
 
+    //p에는 있고, c에는 없는 값을 찾아서 던져주면 된다.
+    //조건 1 : 없는 사람은 단 1명이다 (현재 조건은 그러함)
+    //조건 2 : p에는 중복이 들어올 수 있다. (따라서 set처럼 중복을 없애는 값들은 고려를 해봐야 한다.)
+    public String solution(String[] p, String[] c) {
         if (p.length == 5) {
             return "vinko";
         }
 
-        if (p.length == 4) {
+        if (p.length == 4) { //중복 케이스
             return "mislav";
         }
 
--- jongholee/programmers/hash/java/test/완주하지_못한_선수/SolutionTest.java
+++ jongholee/programmers/hash/java/test/완주하지_못한_선수/SolutionTest.java
@@ -2,6 +2,11 @@ package 완주하지_못한_선수;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
@@ -22,5 +27,34 @@ public class SolutionTest {
         assertEquals("leo", solution.solution(p1, c1));
         assertEquals("vinko", solution.solution(p2, c2));
         assertEquals("mislav", solution.solution(p3, c3));
+    }
+
+    @Test
+    public void testSet() {
+        //set에 키가 중복이 된다면..
+
+        Set set = new HashSet();
+        set.add("a");
+        set.add("b");
+        set.add("b");
+        set.add("c");
+
+        assertEquals("[a, b, c]", set.toString());
+
+        set.remove("c");
+
+        assertEquals("[a, b]", set.toString());
+    }
+
+    @Test
+    public void testMap() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+
+        assertEquals("{a=1, b=2}", map.toString());
+        map.replace("b", map.get("b") + 1);
+        assertEquals("{a=1, b=3}", map.toString());
+        map.replace("b", map.get("b") + 1);
     }
 }
