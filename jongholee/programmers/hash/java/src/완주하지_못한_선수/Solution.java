849d3a9b524ca93863a6b4cf3286edd2a318c0f4
jongholee <jhleed3@gmail.com>
2018-10-06 09:15:36 +0900

    programmers : hash : 완주하지 못한 선수 : 동명이인 인 경우 케이스 처리

--- jongholee/programmers/hash/java/src/완주하지_못한_선수/Solution.java
+++ jongholee/programmers/hash/java/src/완주하지_못한_선수/Solution.java
@@ -9,25 +9,21 @@ public class Solution {
     //p에는 있고, c에는 없는 값을 찾아서 던져주면 된다.
     //조건 1 : 없는 사람은 단 1명이다 (현재 조건은 그러함)
     //조건 2 : p에는 중복이 들어올 수 있다. (따라서 set처럼 중복을 없애는 값들은 고려를 해봐야 한다.)
+
+    //1. 참가자 배열을 -> 맵으로 변환 (값 1로 초기화, 동명이인은 2)
+    //2. 완주자는 뺀다 (값에서 -1을 해서 0이 되면 삭제)
+    //3. 완주하지 못한 사람 색출 남은 값 하나 뽑으면 됨
     public String solution(String[] participants, String[] completers) {
-
-        //1. 참가자 배열을 -> 맵으로 변환 (값 1로 초기화, 동명이인은 2)
-
-        //2. 완주자는 뺀다 (값에서 -1을 해서 0이 되면 삭제)
-
-        //3. 완주하지 못한 사람 색출 남은 값 하나 뽑으면 됨
-
-        if (participants.length == 4) { //중복 케이스
-            return "mislav";
-        }
-
         Map<String, Integer> pMap = new HashMap();
 
         //1. 배열 -> 맵 변환 (값 1로 초기화, 동명이인은 2)
         for (int i = 0; i < participants.length; i++) {
-            //동명이인이 아닌 경우
-            pMap.put(participants[i], 1);
-            //TODO 동명이인 인 경우 고려
+            String key = participants[i];
+            if (pMap.get(key) == null) { //동명이인이 아닌 경우
+                pMap.put(participants[i], 1);
+            } else { //동명이인 인 경우 고려
+                pMap.replace(key, pMap.get(key) + 1);
+            }
         }
 
         //2. 완주자는 뺀다 (값이 1인 경우 삭제)
