bfe143d9d4d02a41f48ba3e5f44de6be1c92cfb9
AsCE-hyunseung <leehs1222@gmail.com>
2018-10-17 01:09:37 +0900

    [level 2] 124 나라의 숫자

---
+++ programmers/level2/124 나라의 숫자/oneTwoFour.java
@@ -0,0 +1,21 @@
+public class oneTwoFour {
+    public String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+        int []countryNumber={4,1,2};
+ 
+        change124(n, answer, countryNumber);
+ 
+        return answer.reverse().toString();//뒤에다 추가해서 reverse()했다.
+    }
+    private void change124(int n, StringBuilder answer, int[] countryNumber) {
+        int check;
+        while (n>0){
+            check=n%3;//나머지
+            n=n/3;//몫
+            if (check==0){//3진수인데 3이 없다.
+                n--;
+            }
+            answer.append(countryNumber[check]);
+        }
+    }
+}
