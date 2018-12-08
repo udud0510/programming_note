6ddc3b95cd96cd02405a3c2d0af4d2ebb951a397
BuildTools <unconfigured@null.spigotmc.org>
2018-12-09 00:37:23 +0900

    1. K번째수

---
+++ sujin/programmers/src/프로그래머스/K번째수/P1208_Test.java
@@ -0,0 +1,21 @@
+package 프로그래머스.K번째수;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class P1208_Test {
+
+    @Test
+    public void main(){
+
+        P1208 solution = new P1208();
+
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3},{4,4,1}};
+        int[] result = {5,6};
+
+        assertArrayEquals(result,solution.solution(array,commands));
+    }
+
+}
