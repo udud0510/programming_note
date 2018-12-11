e6233b08a206e6b04d56ea214209dffc3266927c
udud <susan7744850@gmail.com>
2018-12-11 15:54:17 +0900

    test

---
+++ src/practice/Solution4Test.java
@@ -0,0 +1,18 @@
+package practice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class Solution4Test {
+    @Test
+    public void 결과() {
+        Solution4 solution = new Solution4();
+
+        String[] strings={"sun","bed","car"};
+        int n=1;
+        String[] answer={"car","bed","sun"};
+
+        assertArrayEquals(answer,solution.solution(strings,n));
+    }
+}
